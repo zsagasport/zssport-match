c6538242b6bbb09700d639ad5b9eeffc01d95052
Zsolt Szabo <zsolt.szabo@liferay.com>
2016-11-05 13:18:06 +0100

    Modify demo content based on Match

--- demo/demo.component.ts
+++ demo/demo.component.ts
@@ -2,6 +2,62 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'demo-app',
-  template: '<hello-world></hello-world>'
+  template: `
+     <nav class="navbar navbar-default">
+          <div class="container-fluid">
+              <h1>{{title}}</h1>
+          </div>
+      </nav>
+      <div class="col-md-4">
+             <div class="panel panel-default">
+                <div class="panel-heading">
+                     <h2>Matches type: 'default'</h2>
+                </div>
+                <div class="panel-body">
+                    <zssport-match [matchId]="201617010801"></zssport-match>
+                    <zssport-match [matchId]="201617010802"></zssport-match>
+                    <zssport-match [matchId]="201617010810"></zssport-match>
+                </div>
+            </div>
+        </div>
+       <div class="col-md-4">
+             <div class="panel panel-default">
+                <div class="panel-heading">
+                     <h2>Matches type: 'lined'</h2>
+                </div>
+                <div class="panel-body">
+                    <zssport-match [matchId]="201617010801" [matchType]="'lined'"></zssport-match>
+                    <zssport-match [matchId]="201617010802" [matchType]="'lined'"></zssport-match>
+                    <zssport-match [matchId]="201617010810" [matchType]="'lined'"></zssport-match>
+                </div>
+            </div>
+        </div>
+  `,
+  styles: [
+  `
+      .left {
+          text-align: left;
+      }
+
+      .right {
+          text-align: right;
+      }
+
+      .center {
+          text-align: center;
+      }
+
+      .w-49 {
+          width: 49%;
+      }
+
+      .w-90 {
+          width: 90%;
+      }
+  `
+  ]
 })
-export class Demo {}
+
+export class Demo {
+    title: string = 'Matches';
+}
--- demo/demo.module.ts
+++ demo/demo.module.ts
@@ -1,12 +1,14 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {ZssportMatchModule} from '../src';
+import {MatchService} from '../src/match.service';
+
 import {Demo} from './demo.component';
 
 @NgModule({
   declarations: [Demo],
   imports: [BrowserModule, ZssportMatchModule],
   bootstrap: [Demo],
-  providers: []
+  providers: [MatchService]
 })
 export class DemoModule {}
